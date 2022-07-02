0502ef82c5fc8e4dab1ffc9b54f9fa489ab61e4f
Ethel2 <105896525+Ethel2@users.noreply.github.com>
2022-07-02 10:22:44 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.190.26.13:9000/',
+  serverUrl: 'http://http://3.14.126.45:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': 'b88b4eee82478ac9a64a60fc10fbd82b570529ef',
+	    'sonar.login': '2ddf2645d09b901f4c938d8ea8086dc00da86468',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
